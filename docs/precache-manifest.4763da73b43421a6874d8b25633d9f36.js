097fbeeb390082567ddc06532c51d5ab94265b77
Luis Palacio <guillermo.palacio@onelinkbpo.com>
2020-09-01 09:19:45 -0500

    docs agregado

---
+++ docs/precache-manifest.4763da73b43421a6874d8b25633d9f36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4f8ddaf19134b75f3a23d45be299410",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd46cdd0bb7d58ebf92b",
+    "url": "/static/css/main.888987a8.chunk.css"
+  },
+  {
+    "revision": "9948515d998a14a1480d",
+    "url": "/static/js/2.6a91807f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6a91807f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd46cdd0bb7d58ebf92b",
+    "url": "/static/js/main.94ae4503.chunk.js"
+  },
+  {
+    "revision": "9d3929294d677b1b9ff6",
+    "url": "/static/js/runtime-main.aa9df6a3.js"
+  }
+]);
